90076a50cb12fd7dfd229338a925f54ec15ead32
Nguyễn Lợi <ngtheloi.2710@gmail.com>
2020-08-04 19:22:33 +0700

    hw07 (nhớ test)

---
+++ HW07/app/src/main/java/com/example/hw07/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.hw07;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtText;
+    TextView tvPercent;
+    ProgressBar progressBar;
+    double percent;
+    Button btnDoItAgain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtText = findViewById(R.id.edtText);
+        tvPercent = findViewById(R.id.tvPercent);
+        progressBar = findViewById(R.id.progressBar);
+        btnDoItAgain = findViewById(R.id.btnDoItAgain);
+        progressBar.setProgress(0);
+        progressBar.setMax(100);
+        percent = 0;
+        btnDoItAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initProgressBar();
+            }
+        });
+    }
+
+    private void initProgressBar() {
+        String str = edtText.getText().toString();
+        int value;
+        if (!str.isEmpty()) {
+            value = Integer.valueOf(str);
+        } else {
+            value = 0;
+        }
+        edtText.setEnabled(false);
+        progressBar.setProgress(0);
+        progressBar.setMax(100);
+        percent = 0;
+        btnDoItAgain.setAlpha(0.5f);
+        btnDoItAgain.setEnabled(false);
+        new MyTask().execute(value);
+    }
+
+    private class MyTask extends AsyncTask<Integer, Integer, String> {
+        @Override
+        protected String doInBackground(Integer... params) {
+            double valuePercen = 0;
+            int valueInput = params[0];
+            double value = (float) 1 / valueInput * 100;
+
+            for (int i = 1; i <= valueInput; i++) {
+                try {
+                    Thread.sleep(0);
+                    valuePercen = valuePercen + value;
+                    if (valuePercen >= 1) {
+                        publishProgress((int) valuePercen);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            publishProgress(100);
+            return "Task Completed.";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            btnDoItAgain.setAlpha(1f);
+            btnDoItAgain.setEnabled(true);
+            edtText.setEnabled(true);
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            tvPercent.setText(String.format("%d%%", values[0]));
+            progressBar.setProgress(values[0]);
+        }
+    }
+}
